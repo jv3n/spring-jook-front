259686d328e421e5adc89627712a46fbbdbed583
jv3n <venet.julien@gmail.com>
2024-02-28 22:39:02 -0500

    feat: integrate D3 earth globe.

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Table } from '@shared/components/graphical/table/table.component';
+import { GlobeComponent } from '@shared/components/graphical/globe/globe.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
-  imports: [Table],
+  imports: [Table, GlobeComponent],
 })
 export class HomeComponent {}
